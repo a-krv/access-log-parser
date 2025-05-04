722d695e7a9c8cea927698219b8eb95e8aa1673b
Aleksandr Karavaikin <ache-7@yandex.ru>
2025-05-04 22:00:04 +0300

    added basic math operations of two numbers

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,22 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите первое число:");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
+        int secondNumber = new Scanner(System.in).nextInt();
+
+        int sum = firstNumber + secondNumber;
+        int subtraction = firstNumber - secondNumber;
+        int multiplication = firstNumber * secondNumber;
+        double division = (double) firstNumber / secondNumber;
+
+        System.out.println("Сумма первого и второго числа: " + sum);
+        System.out.println("Разность первого и второго числа: " + subtraction);
+        System.out.println("Произведение первого и второго числа: " + multiplication);
+        System.out.println("Частное первого и второго числа: " + division);
+
+
 
     }
 }
